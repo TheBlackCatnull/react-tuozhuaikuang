659d6dfef316d5570bccc413e407bfb665a9cae9
TheBlackCatnull <67843901+TheBlackCatnull@users.noreply.github.com>
2021-08-30 22:06:46 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import React from "react";
-import "./styles.css";
-import useDraggable from "./useDraggable";
+import "./App.css";
 
+import img3 from "./img/3.jpg";
+import img4 from "./img/4.jpg";
+import img5 from "./img/5.jpg";
+import img6 from "./img/6.jpg";
+import img7 from "./img/7.jpg";
+import img8 from "./img/8.jpg";
+import useDraggable from "./useDraggable";
 const list = [
   {
-    src: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1586970234373&di=a665d347ed7acfed0f39aad0bb78509a&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201504%2F05%2F20150405H5939_PJwYi.jpeg",
-    title: "万事屋找我",
+    src: img3,
+    title: "自来也",
   },
   {
-    title: "吃吃吃……",
-    src: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1586970602470&di=e3071fc352ca96f73bf2e75725d3f7bf&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201208%2F31%2F20120831140113_ayLse.thumb.700_0.jpeg",
+    title: "雏田",
+    src: img4,
   },
   {
-    title: "汪汪",
-    src: "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=968093909,4033932240&fm=26&gp=0.jpg",
+    title: "鸣人",
+    src: img5,
+  },
+  {
+    title: "迪达拉",
+    src: img6,
+  },
+  {
+    title: "卡卡西",
+    src: img7,
+  },
+  {
+    title: "小樱",
+    src: img8,
   },
 ];
 
